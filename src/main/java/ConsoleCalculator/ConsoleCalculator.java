59944bbf667d62dd5a8f2d51aa80409295391d04
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2025-08-25 15:39:47 +0100

    Update ConsoleCalculator.java

--- src/main/java/ConsoleCalculator/ConsoleCalculator.java
+++ src/main/java/ConsoleCalculator/ConsoleCalculator.java
@@ -1,4 +1,39 @@
 package ConsoleCalculator;
 
+
+import java.util.Scanner;
+
 public class ConsoleCalculator {
+    Scanner scan = new Scanner(System.in);
+
+    //first
+//    the add method
+    public static int add (int a , int b){
+        return a + b;
+//        System.out.println(4,5);
+    }
+
+
+//    the subtract method
+    public static int subtract(int a , int b){
+        return a - b;
+    }
+
+//    the multiplication method
+    public static int multiply(int a , int b){
+        return a * b;
+    }
+
+//    the division
+    //divide method doesn't check for division by zero, which will cause an ArithmeticException
+    public static double divide(double a , double b){
+//        return a / b;
+        if(b == 0){
+            System.out.println("Error: Underfined");
+            return 0;
+        }
+        return a /b;
+    }
+
+
 }
