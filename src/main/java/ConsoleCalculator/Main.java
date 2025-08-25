7a1a2f193756be3c837e406c2aefb9b7467201d1
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2025-08-25 15:40:29 +0100

    Update Main.java

--- src/main/java/ConsoleCalculator/Main.java
+++ src/main/java/ConsoleCalculator/Main.java
@@ -1,17 +1,21 @@
 package ConsoleCalculator;
 
+import java.util.Scanner;
+
+import static ConsoleCalculator.ConsoleCalculator.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//        System.out.println(add(5,7));
+//        System.out.println(subtract(58888,6669));
+//        System.out.println(multiply(50,60));
+//        System.out.println( divide(10 , 0));
+
+
     }
+
 }
