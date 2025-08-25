22f35e8d11405e3618bd1d95b428b46fc0bd3fc9
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2025-08-25 16:14:21 +0100

    Update Main.java

--- src/main/java/ConsoleCalculator/Main.java
+++ src/main/java/ConsoleCalculator/Main.java
@@ -1,21 +1,53 @@
 package ConsoleCalculator;
 
+
 import java.util.Scanner;
 
-import static ConsoleCalculator.ConsoleCalculator.*;
+public class ConsoleCalculator {
+    Scanner scan = new Scanner(System.in);
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-//        System.out.println(add(5,7));
-//        System.out.println(subtract(58888,6669));
-//        System.out.println(multiply(50,60));
-//        System.out.println( divide(10 , 0));
-
-
+    //first
+//    the add method
+    public static double add (double  a , double  b){
+        return a + b;
+//        System.out.println(4,5);
     }
+
+
+//    the subtract method
+    public static double  subtract(double  a , double  b){
+        return a - b;
+    }
+
+//    the multiplication method
+    public static double  multiply(double  a , double  b){
+        return a * b;
+    }
+
+//    the division
+    //divide method doesn't check for division by zero, which will cause an ArithmeticException
+    public static double divide(double a , double b){
+//        return a / b;
+        if(b == 0){
+            System.out.println("Error: Underfined");
+            return 0;
+        }
+        return a /b;
+    }
+//    modulus operator   % meaning “remainder.” in real calculator
+//    It returns the remainder of a division operation between two numbers.
+     public static double modulus(double a , double b){
+//        return a / b;
+    if(b == 0){
+        System.out.println("Error: Underfined");
+        return 0;
+    }
+    return a % b;
+}
+//real calculator we have changeSign +/-
+    //only take one parameter and just flip its sign
+public static double  changeSign(double  a , double  b){
+    return -a;
+}
 
 }
